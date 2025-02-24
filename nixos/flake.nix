deac5b3330338630ea3c9cadc107696bb0679cda
test <test@example.com>
2025-02-24 14:54:31 +0000

    inspecting_flakes

--- nixos/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  outputs = { self, nixpkgs }: {
-    # replace 'joes-desktop' with your hostname here.
-    nixosConfigurations.nixos= nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
-    };
-  };
-}
