c0cfeecb3963ed13c573709896ed92c2439025df
System administrator <me@example.com>
2025-04-24 12:14:34 +0100

    post_update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,14 +13,11 @@
     # Include the results of the hardware scan.
     ./modules/all.nix
   ];
+  nix.settings.trusted-users = ["root" "dumbledoor"];
   nix.settings.experimental-features = ["nix-command" "flakes"];
   #AUTO-UPDATE
   system.autoUpgrade.enable = true;
 
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # TODO:
   boot.loader = {
     timeout = 5;
 
@@ -45,14 +42,13 @@
       '';
     };
   };
-  #FIX:-----------
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -79,11 +75,8 @@
     #media-session.enable = true;
   };
 
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # hardware.bluetooth.enable = true; # enables support for Bluetooth
+  # hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.defaultUserShell = pkgs.zsh;
@@ -92,10 +85,6 @@
     isNormalUser = true;
     description = "Dumbledoor";
     extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [
-      kate
-      #  thunderbird
-    ];
   };
   programs.git = {
     enable = true;
@@ -208,29 +197,22 @@
     packer
     # token generation
     openssl
+    # environments
+    devenv
+    # testing
+    postman
+    #hardware listing
+    dmidecode # BIOS/motherboard/RAM/CPU serials
+    lshw # Hardware topology (JSON)
+    inxi # Human-friendly summary
+    util-linux # lsblk
+    pciutils # lspci
+    smartmontools # smartctl
+    jq # JSON processing
+    edid-decode # Decodes EDID (panel model/serial)
+    xorg.xrandr # Screen resolution/refresh rate
+    ddcutil # External monitor control (if supported)
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-  #   services.create_ap = {
-  #   enable = true;
-  #   settings = {
-  #     INTERNET_IFACE = "eth0";
-  #     WIFI_IFACE = "wlan0";
-  #     SSID = "My Wifi Hotspot";
-  #     PASSPHRASE = "12345678";
-  #   };
-  # };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,10 +1,32 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  outputs = { self, nixpkgs }@inputs: {
-    # replace 'joes-desktop' with your hostname here.
-    nixosConfigurations.nixos= nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };  # This passes inputs to your modules
-        modules = [ ./configuration.nix ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11"; # Adjust version as needed
+  };
+  
+  outputs = { self, nixpkgs, nixpkgs-stable }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { 
+        inherit inputs; 
+        # Pass stable pkgs for bluetooth
+        pkgs-stable = import nixpkgs-stable {
+          system = "x86_64-linux"; # Adjust if you use a different architecture
+          config.allowUnfree = true; # Keep this if you need unfree packages
+        };
+      };
+      modules = [ 
+        ./configuration.nix
+        # Add an overlay module to use stable bluetooth
+        ({ pkgs, pkgs-stable, config, ... }: {
+          # Replace bluetooth-related packages with stable versions
+          hardware.bluetooth = {
+            enable = true;
+            package = pkgs-stable.bluez;
+            powerOnBoot = true;
+          };
+          # You might need additional overrides depending on your setup:
+        })
+      ];
     };
   };
 }
