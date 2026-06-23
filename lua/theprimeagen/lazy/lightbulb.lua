f080d9806a9e273b57dfd4e52589794a0fde457f
lmda <lmda@gmail.com>
2026-06-23 02:38:31 +0200

    fix: disable nvim-lightbulb hide_in_unfocused_buffer to prevent E5108 on NvimTree close

--- lua/theprimeagen/lazy/lightbulb.lua
+++ lua/theprimeagen/lazy/lightbulb.lua
@@ -3,6 +3,7 @@ return {
     event = "LspAttach",
     opts = {
         autocmd = { enabled = true, updatetime = 0 },
+        hide_in_unfocused_buffer = false,
         ignore = {
             ft = { "NvimTree", "lazy", "mason", "help", "trouble" },
             bo = { buftype = { "nofile", "terminal", "prompt" } },
